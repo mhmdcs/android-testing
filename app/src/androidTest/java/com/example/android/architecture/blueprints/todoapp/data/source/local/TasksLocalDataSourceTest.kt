3ce2bb59914796440e9194896ef1079f87b2222c
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2021-12-19 10:11:16 +0300

    write another integration test for Room DAO local data source test

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
@@ -55,22 +55,45 @@ class TasksLocalDataSourceTest {
         database.close()
     }
 
+
+    /*
+    * The only real difference between TasksDaoTest and TasksLocalDataSourceTest is that the local data source returns an instance of the sealed Result class,
+    * which is the format the repository expects. For example, in the test below we cast the result as a Success with result as Result.Success
+    * */
+
     //runBlocking has been deprecated. Use its replacement runTest in the future, for now use runBlocking here to avoid libraries compatibility issues. https://github.com/Kotlin/kotlinx.coroutines/issues/1204
     @Test
     fun saveTask_retrievesTask() = runBlocking {
-        // GIVEN - A new task saved in the database.
+        // GIVEN - A new task saved in the database - Creates a task and inserts it into the database.
         val newTask = Task("title", "description", false)
         localDataSource.saveTask(newTask)
 
-        // WHEN  - Task retrieved by ID.
+        // WHEN  - Task retrieved by ID - Retrieves the task using its id.
         val result = localDataSource.getTask(newTask.id)
 
-        // THEN - Same task is returned.
+        // THEN - Same task is returned - Asserts that that task was retrieved, and that all its properties match the inserted task.
         assertThat(result.succeeded, `is`(true))
         result as Result.Success
         assertThat(result.data.title, `is`("title"))
         assertThat(result.data.description, `is`("description"))
         assertThat(result.data.isCompleted, `is`(false))
+    }
+
+    @Test
+    fun completeTask_retrievedTaskIsComplete() = runBlocking {
+        // Given a new task in the persistent repository
+        val newTask = Task("title")
+        localDataSource.saveTask(newTask)
+
+        // When completed in the persistent repository
+        localDataSource.completeTask(newTask)
+        val result = localDataSource.getTask(newTask.id)
+
+        // Then the task can be retrieved from the persistent repository and is complete
+        assertThat(result.succeeded, `is`(true))
+        result as Result.Success
+        assertThat(result.data.title, `is`(newTask.title))
+        assertThat(result.data.isCompleted, `is`(true))
     }
 
 }
